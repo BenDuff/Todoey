72258df2580451b61959c7b2623187ad45ae8980
Benoit Dufort <benoitdufort@Benoits-MacBook-Pro.local>
2018-11-08 20:33:57 -0500

    Add items array saved to local storage

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -12,9 +12,14 @@ class TodoListViewController: UITableViewController {
 
     var itemArray: [String] = []
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let items = defaults.value(forKey: "TodoListItems") as? [String] {
+            itemArray = items
+        }
     }
 
     //MARK TableView Datasource Methods
@@ -50,6 +55,7 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // What will happen once a user click the Add Item Button on our Alert
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "TodoListItems")
             self.tableView.reloadData()
         }
         
